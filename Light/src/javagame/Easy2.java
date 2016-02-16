9ec8989d9224536e9896219403721820abc6a201
Michielu <mich.menning@gmail.com>
2016-02-16 13:35:33 -0600

    Easy2 class

---
+++ Light/src/javagame/Easy2.java
@@ -0,0 +1,31 @@
+package javagame;
+
+import org.newdawn.slick.*; 
+import org.newdawn.slick.state.*;
+import org.newdawn.slick.Image;
+import org.lwjgl.input.Mouse;
+
+//Every screen inherits from BasicGameState. Extends = inherits
+public class Easy2 extends BasicGameState{
+	
+	public Easy2(int state){ 
+		
+	}
+	public void init(GameContainer gc, StateBasedGame sbg)throws SlickException{
+		
+		
+	}
+	
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+		
+	}
+
+	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
+		
+	}
+	
+	
+	public int getID(){
+		return 12;
+	}
+}
