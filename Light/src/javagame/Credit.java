90537c822bdb72e06edb53f7c0762ee5e5a1d6ea
Michielu <mich.menning@gmail.com>
2016-03-05 15:11:13 -0600

    Changes

--- Light/src/javagame/Credit.java
+++ Light/src/javagame/Credit.java
@@ -2,21 +2,22 @@ package javagame;
 
 import org.newdawn.slick.*; 
 import org.newdawn.slick.state.*;
-import org.newdawn.slick.Image;
 import org.lwjgl.input.Mouse;
 
 //Every screen inherits from BasicGameState. Extends = inherits
 public class Credit extends BasicGameState{
+	Image tempImage;
 	public String mouse = "No input yet";
 	public Credit(int state){ 
 		
 	}
 	public void init(GameContainer gc, StateBasedGame sbg)throws SlickException{
-		
+		tempImage = new Image("res/backgrounds/creditexit.png");
 		
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+		tempImage.draw(0, 0);
 		g.drawString(mouse, 50, 50);
 	}
 
@@ -24,6 +25,22 @@ public class Credit extends BasicGameState{
 		int xPos = Mouse.getX();
 		int yPos = Mouse.getY();
 		mouse = "Mouse Position x: " + xPos + " y: " + yPos;
+		Input input = gc.getInput();
+		
+		//As soon as the mouse is pressed, it closes because the same spot has exit on the credits page
+		if((xPos >444&& xPos<650)&& (yPos>214 && yPos < 425)){
+			  if(input.isMouseButtonDown(0)){
+				  System.exit(0);
+			  }
+		  }
+		
+		//This is also temporary; as soon as you click slightly above the exit button in credits, it take you to menu
+		if((xPos >444&& xPos<650) && (yPos<620 && yPos > 440)){
+			//This checks to see if the mouse button is down
+			if(input.isMouseButtonDown(0)){
+				sbg.enterState(0);
+			}
+		}
 	}
 	
 	
