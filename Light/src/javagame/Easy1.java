2885b25c3c73c764d0ec8af0ba733c9e5894c061
Michielu <mich.menning@gmail.com>
2016-03-08 12:54:15 -0600

    this is a commit

--- Light/src/javagame/Easy1.java
+++ Light/src/javagame/Easy1.java
@@ -7,6 +7,7 @@ import org.lwjgl.input.Mouse;
 //Every screen inherits from BasicGameState. Extends = inherits
 public class Easy1 extends BasicGameState{
 	Image gameBoard;
+	Image tileNormal;
 	public String mouse = "No input yet";
 
 	public Easy1(int state){ 
@@ -14,12 +15,29 @@ public class Easy1 extends BasicGameState{
 	}
 	public void init(GameContainer gc, StateBasedGame sbg)throws SlickException{
 		gameBoard = new Image("res/backgrounds/puzzle_background1(1080x720).png");
-		
+		tileNormal = new Image("res/tile/blank_tile(70x70).png");
 	}
 	
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		gameBoard.draw(0,0);
 		g.drawString(mouse, 50, 50);
+		
+		//Image hello = Images.getTile();
+		int xStart = 260;
+		int yStart = 80;
+		int i =0;
+		int j = 0;
+		for(i=0;i<8;i++){
+			for(j=0; j<8;j++){
+				tileNormal.draw(xStart, yStart);
+				yStart = yStart + 70;
+			}
+			xStart = xStart + 70;
+			yStart = 80;
+			
+			
+		}
+		
 	}
 
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
